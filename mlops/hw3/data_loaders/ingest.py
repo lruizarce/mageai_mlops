33bf2bcfe3ecbd0b747d4bcd7d742f4fa0a6e2ed
luisruizarc <messiluisruiz@gmail.com>
2024-06-02 13:42:50 -0600

    created data loader and fixed format

--- mlops/hw3/data_loaders/ingest.py
+++ mlops/hw3/data_loaders/ingest.py
@@ -13,13 +13,13 @@ def ingest_files(**kwargs) -> pd.DataFrame:
     dfs: List[pd.DataFrame] = []
 
     
-        response = requests.get(
+    response = requests.get(
                "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-03.parquet"
             )
 
-        if response.status_code != 200:
-            raise Exception(response.text)
+    if response.status_code != 200:
+        raise Exception(response.text)
 
-        df = pd.read_parquet(BytesIO(response.content))
+    df = pd.read_parquet(BytesIO(response.content))
 
     return df
